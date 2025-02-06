a1c3219d43f1f9a019f9a7f75ecf17105442a6d4
Farnaz Soleimani <f.soleimani@utwente.nl>
2025-02-06 12:30:01 +0100

    updated

--- greeting.py
+++ greeting.py
@@ -1,5 +1,4 @@
 def greetings():
     return "Hello, everyone! Hop you are having a nice day!"
 
-def test_greetings():
-    assert greetings() == "Hello, everyone! Hop you are having a nice day!"
+
---
+++ test_greeting.py
@@ -0,0 +1,3 @@
+import greetings
+def test_greetings():
+    assert greetings() == "Hello, everyone! Hop you are having a nice day!"
